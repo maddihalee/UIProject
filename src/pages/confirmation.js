18f082d1ee061c41c36df95e33e7ba33e6e9e6b3
Maddi Cook <maddihcook@gmail.com>
2024-05-16 19:32:29 -0500

    confirmation and progress bar

--- src/pages/confirmation.js
+++ src/pages/confirmation.js
@@ -1,5 +1,10 @@
 export default function Confirmation() {
   return (
+    <div className="box">
+       <div className="progress">
+        Step 3/3
+      </div>
     <h1>You have completed your account setup!</h1>
+    </div>
   )
 }
